b0497028eb1fd52f591f9cd64d03109b7051512d
ToF- <cthibauttof@gmail.com>
2026-03-31 21:27:02 +0200

    strikes

--- bowling/lib/bowling.ml
+++ bowling/lib/bowling.ml
@@ -1,5 +1,6 @@
 
 let rec score throws = match throws with
 | [] -> 0
+| x :: y :: z :: zs when x = 10 -> x + y + z + score (y :: z :: zs)
 | x :: y :: z :: zs when x + y = 10 -> x + y + z + score (z :: zs)
 | x:: xs -> x + (score xs)
--- bowling/test/test_bowling.ml
+++ bowling/test/test_bowling.ml
@@ -2,11 +2,16 @@ open OUnit2
 
 let suite =
     "bowling tests" >::: [
-        "initially zero" >:: (fun _ -> assert_equal 0 (Bowling.score [])) ;
-        "a single non strike throw is added" >:: (fun _ -> assert_equal 8 (Bowling.score [8]));
-        "average throws are added" >:: (fun _ -> assert_equal 13 (Bowling.score [5; 2; 6])) ; 
-        "a spare adds the next throw as bonus" >:: (fun _ -> assert_equal 14 (Bowling.score [3; 7; 2])) ;
-        
+        "initially zero" >:: 
+            (fun _ -> assert_equal 0 (Bowling.score [])) ;
+        "a single non strike throw is added" >:: 
+            (fun _ -> assert_equal 8 (Bowling.score [8]));
+        "average throws are added" >:: 
+            (fun _ -> assert_equal 13 (Bowling.score [5; 2; 6])) ; 
+        "a spare adds the next throw as bonus" >:: 
+            (fun _ -> assert_equal 14 (Bowling.score [3; 7; 2])) ;
+        "a strike addt the next two throws as bonus" >:: 
+            (fun _ -> assert_equal 24 (Bowling.score [10; 3; 3; 2])) ;
     ]
 let () =
   run_test_tt_main suite
