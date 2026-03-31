67817f598f2a61cf4ec1dea4bab585a862f5308a
ToF- <cthibauttof@gmail.com>
2026-03-31 18:45:10 +0200

    # Previous commit messages: # -37aee7d initial commit #

--- bowling/lib/bowling.ml
+++ bowling/lib/bowling.ml
@@ -1,2 +1,5 @@
 
-let score a = 0
+let rec score throws = match throws with
+| [] -> 0
+| x :: y :: z :: zs when x + y = 10 -> x + y + z + score (z :: zs)
+| x:: xs -> x + (score xs)
--- bowling/test/test_bowling.ml
+++ bowling/test/test_bowling.ml
@@ -1,11 +1,12 @@
 open OUnit2
 
-let score_with_zero_throws_is_zero _ =
-  assert_equal 0 (Bowling.score 0)
-
 let suite =
     "bowling tests" >::: [
-        "initially zero" >:: score_with_zero_throws_is_zero;
+        "initially zero" >:: (fun _ -> assert_equal 0 (Bowling.score [])) ;
+        "a single non strike throw is added" >:: (fun _ -> assert_equal 8 (Bowling.score [8]));
+        "average throws are added" >:: (fun _ -> assert_equal 13 (Bowling.score [5; 2; 6])) ; 
+        "a spare adds the next throw as bonus" >:: (fun _ -> assert_equal 14 (Bowling.score [3; 7; 2])) ;
+        
     ]
 let () =
   run_test_tt_main suite
