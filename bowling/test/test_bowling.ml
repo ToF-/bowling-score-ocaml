f59417b47b775402be453bfb0aa4639ccd0f2bac
ToF- <cthibauttof@gmail.com>
2026-04-04 13:01:23 +0200

    # Previous commit messages: # -786c830 # Previous commit messages: # -73100b0 added some dummy qcheck tests # -59d75d5 refactoring # -2483985 perfect # -b049702 strikes # -67817f5 # Previous commit messages: # -37aee7d initial commit # # # -73100b0 added some dummy qcheck tests # -59d75d5 refactoring # -2483985 perfect # -b049702 strikes #

--- bowling/test/test_bowling.ml
+++ bowling/test/test_bowling.ml
@@ -17,21 +17,40 @@ let unit_tests =
             (fun _ -> assert_equal ~printer:string_of_int 300 (Bowling.score [10; 10; 10; 10; 10; 10; 10; 10; 10; 10; 10; 10])) ;
     ]
 
-let passing =
-  QCheck.Test.make ~count:1000
-    ~name:"list_rev_is_involutive"
-    QCheck.(list nat_small)
-    (fun l -> List.rev (List.rev l) = l);;
+let bowling_list_gen =
+  let open Gen in
 
-let failing =
-  QCheck.Test.make ~count:10
-    ~name:"fail_sort_id"
-    QCheck.(list nat_small)
-    (fun l -> List.sort compare l = List.sort compare l);;
+  (* generate one frame (1 or 2 throws) *)
+  let frame_gen =
+    let* first = int_range 0 10 in
+    if first = 10 then
+      (* strike: only one throw in frame *)
+      return [10]
+    else
+      let* second = int_range 0 (10 - first) in
+      return [first; second]
+  in
+
+  (* build up to 10 frames, flatten, then truncate to 20 throws *)
+  let rec frames n =
+    if n = 0 then return []
+    else
+      let* f = frame_gen in
+      let* rest = frames (n - 1) in
+      return (f @ rest)
+  in
+
+  let* all_throws = frames 10 in
+  return (List.take 20 all_throws)
+
+  let gen = make bowling_list_gen
+let score_is_less_or_equal_200 =
+    Test.make  ~name: "score <= 200"
+    gen (fun rolls -> Bowling.score rolls <= 200)
 
 let _ =
     List.map run_test_tt_main [
             unit_tests ;
-           ("tests" >::: List.map QCheck_ounit.to_ounit2_test [passing; failing]) ;
+           ("tests" >::: List.map QCheck_ounit.to_ounit2_test [score_is_less_or_equal_200]) ;
     ]
 
